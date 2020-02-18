6e132f53d4d87a3d475c7f5ce34f68ba9cac844c
surajkamblesk <alphacoderofficial@gmail.com>
2020-02-19 00:29:06 +0530

    stylized the edittext and button using xml

--- BMI_Calculator/app/src/main/java/com/deepconnect/bmi_calculator/MainActivity.java
+++ BMI_Calculator/app/src/main/java/com/deepconnect/bmi_calculator/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +37,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
+
 
     private void BindViews() {
         input_height = findViewById(R.id.ET_height);
